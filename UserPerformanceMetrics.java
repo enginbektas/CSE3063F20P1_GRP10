be038be5368c91d19dc60aec95a19806ee065952
enginbektas <74172356+enginbektas@users.noreply.github.com>
2020-12-16 22:21:28 +0300

    userperformancemetrics push

---
+++ UserPerformanceMetrics.java
@@ -0,0 +1,17 @@
+public class UserPerformanceMetrics {
+    private int datasetAssigned;
+    private float datasetCompleteness;
+    private int numberOfInstancesLabeled;
+    private int numberOfUniqueInstancesLabeled;
+    private double consistencyPercentage;
+    private double averageTimeSpentLabeling;
+    private double stdDevOfTimeSpentLabelingInstances;
+
+    public UserPerformanceMetrics() {
+
+    }
+
+
+
+}
+
