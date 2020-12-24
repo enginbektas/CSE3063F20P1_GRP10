7e76114f472e7b030c2a0df22b9bdb725f8c6aef
Mehmet Ali Gözükızıl <35894331+maligzkzl@users.noreply.github.com>
2020-12-24 19:25:40 +0300

    Comments are added.

--- DatasetPerformanceMetric.java
+++ DatasetPerformanceMetric.java
@@ -17,6 +17,7 @@ public class DatasetPerformanceMetric {
         this.consistencyPercentageOfUsers = new ArrayList<>();
     }
 
+    //update dataset performance metric
     public void update(){
         this.calculateCompleteness();
         this.calculateConsistencyPercentageOfUsers();
@@ -26,10 +27,12 @@ public class DatasetPerformanceMetric {
         this.calculateUsersCompleteness();
 
     }
-
+    //calculate completeness percentage using instances
     private void calculateCompleteness(){
         this.completenessPercentage = 0;
+        //for all instances of dataset
         for (Instance instance: dataset.getInstances() ) {
+            //if instance has a label
             if(instance.getLabels().size() != 0){
                 this.completenessPercentage++;
             }
@@ -38,30 +41,35 @@ public class DatasetPerformanceMetric {
         this.completenessPercentage *= 100;
 
     }
-
+    //calculate label distribution using labels
     private void calculateLabelDistribution(){
+        //clear array
         this.labelDistributionPercentage = new ArrayList<>();
         int totalUsage = 0;
+        //calculate total usage of labels
         for (Label label: dataset.getLabels()) {
             totalUsage += label.getNumberOfUses();
         }
-
         if (totalUsage == 0){
             for (Label label: dataset.getLabels()) {
                 this.labelDistributionPercentage.add(new Percentage(label.getText(), 0));
             }
         }
-
+        //for every label calcualte and add values
         for (Label label: dataset.getLabels()) {
             this.labelDistributionPercentage.add(new Percentage(label.getText(), label.getNumberOfUses()  / (totalUsage * 1.0)));
         }
     }
-
+    //calculate number of unique instances for each label using labels and instances
     private void calculateNumberUniqueInstancesForEachLabel(){
+        //clear array
         this.numberUniqueInstancesForEachLabel = new ArrayList<>();
+        //for every label of dataset
         for (Label label: dataset.getLabels()) {
             int numberOfUniqueInstances = 0;
+            //for every instance
             for (Instance instance: dataset.getInstances()) {
+                //check unique instance
                 if(instance.getLabels().contains(label)){
                     numberOfUniqueInstances++;
                 }
@@ -69,18 +77,20 @@ public class DatasetPerformanceMetric {
             this.numberUniqueInstancesForEachLabel.add(new Percentage(label.getText(), numberOfUniqueInstances));
         }
     }
-
+    //calculate number of users
     private void calculateNumberOfUsers(){
         numberOfUsers = dataset.getUsers().size();
     }
-
-
-    //TODO User Metric Required
+    //calculate user completeness using users' performance metric
     private void calculateUsersCompleteness(){
+        //clear array
         this.usersCompleteness = new ArrayList<>();
+        //for every user of dataset
         for (User user: dataset.getUsers()) {
             double percentage = 0;
+            //for every user completeness in user metric
             for (int i = 0; i < user.getUserPerformanceMetrics().getUsersCompleteness().size(); i++) {
+                //find current dataset in user completeness
                 if (user.getUserPerformanceMetrics().getUsersCompleteness().get(i).getName().equals(dataset.getId() + "")){
                     this.usersCompleteness.add( new Percentage(user.getUserName(), user.getUserPerformanceMetrics().getUsersCompleteness().get(i).getPercentage()));
                 }
@@ -88,9 +98,11 @@ public class DatasetPerformanceMetric {
 
         }
     }
-    //TODO User Metric Required
+    //calculate consistency percentage using users' performance metric
     private void calculateConsistencyPercentageOfUsers(){
+        //clear array
         consistencyPercentageOfUsers = new ArrayList<>();
+        //get consistency percentage from users
         for (User use: dataset.getUsers()) {
             consistencyPercentageOfUsers.add(new Percentage(use.getUserName(), use.getUserPerformanceMetrics().getConsistencyPercentage().getPercentage()));
         }
