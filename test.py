0e9778ace507c18cb76829832aad1e59fedccf60
Enes <nasyrest@hotmail.com>
2021-01-11 16:37:08 +0300

    16 36

--- test.py
+++ test.py
@@ -4,15 +4,7 @@ import pandas
 # Başlık; netten buldum aşağıdkai kodu, row ları vs değiştirmmiz lazım
 #MALİ:
 #ENGİN;
-# MAHMUT;
-with open('CSE3063_20201123_Mon_zoom_PollReport.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+#MAHMUT;nereyi eksik basıyor? Netim iyi değil guys
+
+df = pandas.read_csv('excel files/CSE3063_20201123_Mon_zoom_PollReport.csv')
+print(df.to_string())
