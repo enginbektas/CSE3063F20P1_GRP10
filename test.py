fb1f681101baab7b788000d98d58cd89836e6ac5
enginbektas <74172356+enginbektas@users.noreply.github.com>
2021-01-11 18:38:39 +0300

    reader is being implemented

--- test.py
+++ test.py
@@ -4,7 +4,40 @@ import pandas
 # Başlık; netten buldum aşağıdkai kodu, row ları vs değiştirmmiz lazım
 #MALİ:
 #ENGİN;
-#MAHMUT;nereyi eksik basıyor? Netim iyi değil guys
+#HASAN:
+#MAHMUT;
 
-df = pandas.read_csv('excel files/CSE3063_20201123_Mon_zoom_PollReport.csv')
-print(df.to_string())
+df = pandas.read_csv("excel files/CSE3063_20201123_Mon_zoom_PollReport.csv", keep_default_na=False)
+# del df['^Unnamed']
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+df.fillna('')
+# df = df.T
+
+for index, row in df.iterrows():
+    #TODO  Create an object of 'student'
+    print(index[1] + " " + index[2] + " " +  index[3])
+    i = 4
+    #for i in range(0, len(test_list)):
+    while(True):
+        try:
+            # TODO Create an object of 'answer'
+            if len(index[i]) == 0:
+                break
+            print("Question = " + index[i])
+            print("Answer = " + index[i+1])
+            i += 2
+        except IndexError:
+            break
+            'var not defined'
+
+
+
+  ##print(row)
+
+print(df.shape)
+
+#print(df(0))
+
+#print(df['#'].to_string())
+
+
