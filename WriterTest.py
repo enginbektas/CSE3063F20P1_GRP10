c87b2e0c6f1be3c809391872bcd501931e481b88
ouzhanerkol <oguzhannerkol@gmail.com>
2021-01-16 23:50:28 +0300

    Add create_global_output!

--- WriterTest.py
+++ WriterTest.py
@@ -1,7 +1,7 @@
 import xlsxwriter
 from openpyxl import load_workbook
 import pyexcel as p
-import os.path
+
 from os import path
 
 # attandance output need student excel
@@ -45,8 +45,8 @@ worksheet.insert_chart('H2', chart2, {'x_offset': 0, 'y_offset': 0})
 workbook.close()
 
 
-def createAttendanceOutput(studentList):
-    if path.exits('excel files/CSE3063_Fall2020_att_SinifListesi.xlsx'):
+def create_attendance_output(student_list):
+    if path.exists('excel files/CSE3063_Fall2020_att_SinifListesi.xlsx'):
         pass
     else:
         p.save_book_as(file_name='excel files/CES3063_Fall2020_rptSinifListesi.xls',
@@ -58,7 +58,7 @@ def createAttendanceOutput(studentList):
     ws['N13'] = "Attendance polls"
     ws['P13'] = "Attendance rate"
     ws['R13'] = "Attendance percentage"
-    for student in studentList:
+    for student in student_list:
         ws['N' + str(i)] = str(student.get_totalAttendance())
         ws['P' + str(i)] = str(student.get_attendance()) + " of " + str(student.get_totalAttendance())
         ws['Q' + str(i)] = str(student.get_attendance() / student.get_totalAttendance() * 100)
@@ -67,8 +67,8 @@ def createAttendanceOutput(studentList):
     wb.save('CSE3063_Fall2020_att_SinifListesiAttendence.xlsx')
 
 
-def createPollOutput(studentList, poll):
-    if path.exits('excel files/CSE3063_Fall2020_' + poll.get_name() + '.xlsx'):
+def create_poll_output(student_list, poll):
+    if path.exists('excel files/CSE3063_Fall2020_' + poll.get_name() + '.xlsx'):
         pass
     else:
         p.save_book_as(file_name='excel files/CSE3063_Fall2020_rptSinifListesi.xls',
@@ -76,66 +76,75 @@ def createPollOutput(studentList, poll):
 
     wb = load_workbook('excel files/CSE3063_Fall2020_' + poll.get_name() + '.xlsx')
     ws = wb.worksheets[0]
-    lenPoll = len(poll.get_questions())
+    len_poll = len(poll.get_questions())
 
     i = 14
-    columnChr = 78
-    for j in lenPoll:
-        ws[chr(columnChr) + '13'] = "Q" + str(j)
+    column_chr = 78
+    for j in range(1, len_poll + 1):
+        ws[chr(column_chr) + '13'] = "Q" + str(j)
         j += 1
-        columnChr += 1
-    successChr = columnChr
-    columnChr = 78
-    ws[chr(successChr) + '13'] = "Number of questions"
-    ws[chr(successChr + 1) + '13'] = "Success Percentage"
+        column_chr += 1
+    success_chr = column_chr
+    column_chr = 78
+    ws[chr(success_chr) + '13'] = "Number of questions"
+    ws[chr(success_chr + 1) + '13'] = "Success Percentage"
 
-    for student in studentList:
-        answeredPoll = None
+    for student in student_list:
+        answered_poll = None
         for ap in student.get_answered_polls:
             if poll.get_name() == ap.get_poll().get_name():
-                answeredPoll = ap
+                answered_poll = ap
                 break
         count = 0
-        correctAnswer = 0
-        while successChr > columnChr:
-            if answeredPoll.get_answer(poll.get_questions()[count]) == poll.get_questions()[count].get_trueChoice:
-                ws[chr(columnChr) + str(i)] = "1"
-                correctAnswer += 1
+        correct_answer = 0
+        while success_chr > column_chr:
+            if answered_poll.get_answer(poll.get_questions()[count]) == poll.get_questions()[count].get_trueChoice:
+                ws[chr(column_chr) + str(i)] = "1"
+                correct_answer += 1
             else:
-                ws[chr(columnChr) + str(i)] = "0"
-            columnChr += 1
+                ws[chr(column_chr) + str(i)] = "0"
+            column_chr += 1
             count += 1
-        ws[chr(successChr) + str(i)] = str(successChr - columnChr)
-        ws[chr(successChr + 1) + str(i)] = str(correctAnswer / (successChr - columnChr) * 100)
+        ws[chr(success_chr) + str(i)] = str(success_chr - column_chr)
+        ws[chr(success_chr + 1) + str(i)] = str(correct_answer / (success_chr - column_chr) * 100)
         i += 1
 
 
-def createGlobalOutput(studentList, poll):
-    if path.exits('excel files/CSE33063_Fall2020_glbSinifListesi.xlsx'):
+def create_global_output(student_list, poll_list):
+    if path.exists('excel files/CSE33063_Fall2020_glbSinifListesi.xlsx'):
         pass
     else:
         p.save_book_as(file_name='excel files/CSE3063_Fall2020_rptSinifListesi.xls',
                        dest_file_name='excel files/CSE33063_Fall2020_glbSinifListesi.xlsx')
     wb = load_workbook('excel files/CSE33063_Fall2020_glbSinifListesi.xlsx')
     ws = wb.worksheets[0]
-    ws['N13'] = "q1_name"
-    ws['P13'] = "q1_numOfQuestions"
-    ws['R13'] = "q1_successRate"
+    column_chr = 78
 
-    i = 14
-    for student in studentList:
-        ws['N' + str(i)] = str(poll.get_name())
-        ws['P' + str(i)] = str(len(poll.get_questions()))
-        ws['Q' + str(i)] = "25" #success
+    j = 0
+    for poll in poll_list:
+        first_column = chr(column_chr)
+        second_column = chr(column_chr + 1)
+        third_column = chr(column_chr + 2)
+        ws[first_column + '13'] = "q" + str(j) + "_name"
+        ws[second_column + '13'] = "q" + str(j) + "_numOfQuestions"
+        ws[third_column + '13'] = "q" + str(j) + "successRate"
 
-        answeredPoll = None
-        for ap in student.get_answered_polls:
-            if poll.get_name() == ap.get_poll().get_name():
-                answeredPoll = ap
-                break
+        i = 14
+        for student in student_list:
+            ws[first_column + str(i)] = str(poll.get_name())
+            ws[second_column + str(i)] = str(len(poll.get_questions()))
 
-        for q in poll.get_questions:
-            if answeredPoll.get_answer(q) == q.get_trueChoice:
-                correctAnswer += 1
-
-        i += 1
+            answered_poll = None
+            correct_answer = 0
+            num_of_questions = 0
+            for ap in student.get_answered_polls:  # to find to student answer
+                if poll.get_name() == ap.get_poll().get_name():
+                    answered_poll = ap
+                    break
+            for q in poll.get_questions:  # find to number of questions and correct answer
+                num_of_questions += 1
+                if answered_poll.get_answer(q) == q.get_trueChoice:
+                    correct_answer += 1
+            ws[third_column + str(i)] = str(correct_answer / num_of_questions * 100)
+            i += 1
+        column_chr += 3
